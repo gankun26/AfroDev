0f65fc10702594c3c0efa4f25a46682fbd3f5f9e
alan santana <alan.nsantana@hotmail.com>
2021-04-01 22:10:32 -0300

    quarto dia de aula

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,18 @@
+const Agendamento = require('../models/Agendamento');
 
 module.exports = app =>{
     app.get('/agendamentos', (req, resp) =>{
-        resp.send('servidor voando, igual batman em dia de lua cheia')
+        Agendamento.Listagem(resp);
     });
+
+    app.post('/agendamentos', (req, resp) => {
+        const agendamento = req.body;
+        Agendamento.inserir(agendamento, resp);
+        
+    });
+    app.get('/agendamentos/:id',(req, resp) =>{
+        const id = parseInt(req.params.id)
+        Agendamento.buscarPorId(id, resp)
+    })
 };
 
